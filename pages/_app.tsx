210018e2cd91ae0f0d24b3cc38cd6c23b057dff4
Kristián Žuffa <kristian.zuffa@gmail.com>
2021-05-12 22:07:23 +0200

    assets

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import Head from "next/head";
-import {CssBaseline, MuiThemeProvider} from "@material-ui/core";
-import {createTheme} from "../src/theme/DefaultTheme";
+import Head from 'next/head';
+import { CssBaseline, MuiThemeProvider } from '@material-ui/core';
+import { createTheme } from '../src/theme/DefaultTheme';
 
 interface AppProps {
   Component: React.ComponentType<any>;
@@ -9,7 +9,7 @@ interface AppProps {
 }
 
 export default function App(props: AppProps) {
-  const {Component, pageProps} = props;
+  const { Component, pageProps } = props;
   const theme = React.useMemo(() => createTheme(), []);
   React.useEffect(() => {
     // Remove the server-side injected CSS.
@@ -22,14 +22,23 @@ export default function App(props: AppProps) {
     <>
       <Head>
         <title>Patria</title>
-        <link rel="preconnect" href="https://fonts.gstatic.com"/>
-        <link href="https://fonts.googleapis.com/css2?family=Comfortaa:wght@300;400;500;600;700&display=swap"
-              rel="stylesheet"/>
+        <link rel='preconnect' href='https://fonts.gstatic.com' />
+        <link href='https://fonts.googleapis.com/css2?family=Comfortaa:wght@300;400;500;600;700&display=swap'
+              rel='stylesheet' />
+        <link rel='apple-touch-icon' sizes='180x180' href='/img/favicon/apple-touch-icon.png' />
+        <link rel='icon' type='image/png' sizes='32x32' href='/img/favicon/favicon-32x32.png' />
+        <link rel='icon' type='image/png' sizes='16x16' href='/img/favicon/favicon-16x16.png' />
+        <link rel='manifest' href='/img/favicon/site.webmanifest' />
+        <link rel='mask-icon' href='/img/favicon/safari-pinned-tab.svg' color='#d65a31' />
+        <link rel='shortcut icon' href='/img/favicon/favicon.ico' />
+        <meta name='msapplication-TileColor' content='#d65a31' />
+        <meta name='msapplication-config' content='/img/favicon/browserconfig.xml' />
+        <meta name='theme-color' content='#ffffff' />
       </Head>
       <MuiThemeProvider theme={theme}>
-        <CssBaseline/>
-        <Component {...pageProps}/>
+        <CssBaseline />
+        <Component {...pageProps} />
       </MuiThemeProvider>
     </>
-  )
+  );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,32 +1,8 @@
 import React from 'react';
-import {MainLogo} from "../src/ui/main-logo/MainLogo";
-import {Box, Checkbox, FormControlLabel, Radio, RadioGroup} from "@material-ui/core";
-import {RoundedLogo} from "../src/ui/rounded-logo/RoundedLogo";
 
 export default function Main() {
-  const [larger, setLarger] = React.useState(false);
-  const [image, setImage] = React.useState<string>();
   return (
     <>
-      <div style={{background: '#fff', padding: 10}}>
-        <FormControlLabel checked={larger}
-                          control={<Checkbox/>}
-                          label="Larger"
-                          onChange={(e, checked) => setLarger(checked)}/>
-        <RadioGroup name="image" value={image} onChange={(e) => setImage(e.target.value === 'none' ? undefined : e.target.value as any)}>
-          <FormControlLabel value="none" control={<Radio />} label="None" />
-          <FormControlLabel value="first-wheel" control={<Radio />} label="1st" />
-          <FormControlLabel value="second-wheel" control={<Radio />} label="2nd" />
-        </RadioGroup>
-      </div>
-      <Box display="flex" flexDirection="row">
-        <MainLogo larger={larger} img={image}/>
-        <MainLogo larger={larger} img={image} contrast/>
-      </Box>
-      <Box display="flex" flexDirection="row">
-        <RoundedLogo larger={larger} img={image}/>
-        <RoundedLogo larger={larger} img={image} contrast/>
-      </Box>
     </>
-  )
+  );
 }
